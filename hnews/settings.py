0374e3428eb3293f3a6620125b2dfd58213fc6b4
JFP3 <jfpardini@gmail.com>
2013-12-12 15:28:53 +0100

    Second tutorial completed

--- hnews/settings.py
+++ hnews/settings.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.core.urlresolvers import reverse_lazy
+
+LOGIN_URL=reverse_lazy('login')
+LOGIN_REDIRECT_URL = reverse_lazy('home')
+LOGOUT_URL=reverse_lazy('logout')
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -38,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'links',
     'hnews',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
--- hnews/urls.py
+++ hnews/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-from links.views import LinkListView
+from django.contrib.auth.decorators import login_required as auth
+from links.views import LinkListView, UserProfileDetailView, UserProfileEditView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,4 +8,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', LinkListView.as_view(), name='home'),
+    url(r'^login/$', 'django.contrib.auth.views.login', { 'template_name': 'login.html' }, name="login"),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name="logout"),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^users/(?P<slug>\w+)/$', UserProfileDetailView.as_view(), name="profile"),
+    url(r'^edit_profile/$', auth(UserProfileEditView.as_view()), name="edit_profile"),
 )
